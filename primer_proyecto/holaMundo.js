acbbd978ab6eebb209cd3a907f3840dbd6f84a97
Facundo Lubo <facundolubo@gmail.com>
2022-12-14 18:16:14 -0300

    Primer Commit

---
+++ primer_proyecto/holaMundo.js
@@ -0,0 +1,2 @@
+let string = "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
+console.log('string :>> ', string);
---
+++ primer_proyecto/index.js
@@ -0,0 +1,2 @@
+//Saludo inicial
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
